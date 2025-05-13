149b618dd780b0bbebd674cdea716763a0cb218f
Paumaro92 <paumr92@hotmail.es>
2025-05-13 13:24:36 +0200

    Añadido ejercicio 1.02

---
+++ s1_02_exercici1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ca">
+    <head>
+        <meta charset="UTF-8">
+        </head>
+<header>
+    <body>
+
+<?php
+$numEnt = 10; //integer
+$decimal = 1.5; //double
+$helloWorld = "Hola món"; //string
+$x = true; //boolean
+const NOM = "Pau";
+
+echo "<h1>" . NOM . "</h1>\n";
+echo "<p>$numEnt</p>\n";
+echo "<p>$decimal</p>\n";
+echo "<p>$helloWorld</p>\n";
+echo "<p>$x</p>\n";
+?>
+        </header>
+    </body>
+</html>
